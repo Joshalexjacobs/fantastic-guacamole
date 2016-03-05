76f0cd3efaba3c6f7b28b37ee17157044ced4aeb
Jay17 <joshjacobs007@gmail.com>
2016-03-04 20:13:24 -0800

    fixed scanlines in env.lua

--- environment.lua
+++ environment.lua
@@ -6,12 +6,12 @@ local function drawScanLines()
 
   local columnWidth = 800 / 200
   for x=1, 200 do
-    love.graphics.line(x*columnWidth, 0, x*columnWidth, 800)
+    love.graphics.line(x * columnWidth, 0, x * columnWidth, 800)
   end
 
   local rowHeight = 600 / 150
   for y=1, 150 do
-    love.graphics.line(0, y*rowHeight, 600, y*rowHeight)
+    love.graphics.line(0, y * rowHeight, 800, y * rowHeight)
   end
 
   setColor(r, g, b, a)
